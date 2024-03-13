74d3dc90c32d0a1e32a9d537cadff9d6a04fb2bc
MIROCHNYK <mirochnyk.sergiy@gmail.com>
2024-03-13 20:59:29 +0100

    correction of columns and rows

--- hackSpot.js
+++ hackSpot.js
@@ -1,7 +1,8 @@
 
 const ExcelJS = require('exceljs');
 
-async function removeColumns() {
+
+async function clear() {
 	const workbook = new ExcelJS.Workbook();
 	await workbook.xlsx.readFile('../hack_excel/CIU Table.xlsx');
 	const worksheet = workbook.getWorksheet('Spot');
@@ -17,58 +18,56 @@ async function removeColumns() {
 	worksheet.spliceColumns(1, 1); // Удаляем колонку A
 	worksheet.spliceColumns(4, 1); // Удаляем колонку D, которая теперь представляет собой бывшую E
 
-	// Очистка содержимого в первых двух новых строках (если требуется)
+	// Очистка содержимого в первых двух новых строках
 	for (let col = 1; col <= worksheet.columnCount; col++) {
-	  worksheet.getRow(1).getCell(col).value = null;
-	  worksheet.getRow(2).getCell(col).value = null;
+		 worksheet.getRow(1).getCell(col).value = null;
+		 worksheet.getRow(2).getCell(col).value = null;
 	}
 
+	// Очистка границ всех ячеек на листе
+	worksheet.eachRow({ includeEmpty: true }, function(row) {
+		row.eachCell({ includeEmpty: true }, function(cell) {
+			 cell.border = {
+				  top: { style: 'thin', color: { argb: 'FFD3D3D3' } },
+				  left: { style: 'thin', color: { argb: 'FFD3D3D3' } },
+				  bottom: { style: 'thin', color: { argb: 'FFD3D3D3' } },
+				  right: { style: 'thin', color: { argb: 'FFD3D3D3' } }
+			 };
+		});
+  });
+  
 	await workbook.xlsx.writeFile('../hack_excel/CIU_Table_modified.xlsx');
 	return { workbook, worksheet }; // Возвращаем workbook и worksheet для дальнейшего использования
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 //////////////////////////////////////////////////////////////////////////////////
-async function mergeCells(worksheet) {
-  const mergeRanges = [
-    'A1:A2', 'B1:B2', 'C1:C2', 'D1:D2', 'E1:F1', 'F1:G1', 'H1:K1', 'L1:L2', 'M1:M2', 'N1:N2', 'O1:O2'
-  ];
+function setBordersForNonEmptyCells(worksheet) {
+	worksheet.eachRow({ includeEmpty: true }, function(row) {
+		 row.eachCell({ includeEmpty: true }, function(cell) {
+			  if (cell.value !== null && cell.value !== '') {
+					cell.border = {
+						 top: { style: 'thin', color: { argb: 'FF000000' } },
+						 left: { style: 'thin', color: { argb: 'FF000000' } },
+						 bottom: { style: 'thin', color: { argb: 'FF000000' } },
+						 right: { style: 'thin', color: { argb: 'FF000000' } }
+					};
+			  }
+		 });
+	});
+}
 
-  mergeRanges.forEach(range => {
-    try {
-      worksheet.mergeCells(range);
-    } catch (error) {
-      console.log(`Ошибка при объединении диапазона ${range}: ${error}`);
-    }
-  });
-
-  mergeRanges.forEach(range => {
-    worksheet.getCell(range.split(':')[0]).border = {
-      top: { style: 'thin' },
-      left: { style: 'thin' },
-      bottom: { style: 'thin' },
-      right: { style: 'thin' }
-    };
-  });
-}  
+async function draw(worksheet) {
+	// Так как `setBordersForNonEmptyCells` уже определена, просто вызываем её с текущим листом
+	setBordersForNonEmptyCells(worksheet);
+}
+ //////////////////////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////////////////////
 async function hackSpot() {
-  const { workbook, worksheet } = await removeColumns();
-  await mergeCells(worksheet);
-  await workbook.xlsx.writeFile('../hack_excel/CIU_Table_modified_final.xlsx'); // Сохраняем окончательные изменения
+	const { workbook, worksheet } = await clear();
+	await draw(worksheet); // Здесь мы используем уже модифицированный worksheet из `clear`
+	await workbook.xlsx.writeFile('../hack_excel/CIU_Table_modified_final.xlsx'); // Сохраняем окончательные изменения
 }
-hackSpot().then(() => console.log('Лишние колонки и строки удалены, ячейки объединены 😎'));
+hackSpot().then(() => console.log('Лишние колонки и строки удалены 😎'));
+
